23fc81bddf2f2baf2c7386fb070d9aa1c4e6d31e
bigassopper <deathdiec@gmail.com>
2021-10-03 21:58:17 -0300

    level 2 finished

--- Assets/Scripts/Objects/Saw.cs
+++ Assets/Scripts/Objects/Saw.cs
@@ -8,6 +8,11 @@ public class Saw : DamagePlayer
     public float speed;
     private int current = 0;
     [SerializeField] private float wRadius = .5f;
+    private void Start() {
+        foreach(var w in waypoints){
+            w.transform.parent = null;
+        }
+    }
     private void Update()
     {
         if(Vector2.Distance(waypoints[current].transform.position, transform.position) < wRadius)
