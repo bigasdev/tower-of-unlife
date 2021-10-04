3290e5bbf9533bd27924c9d589a394b98c6aa8fc
bigassopper <deathdiec@gmail.com>
2021-10-03 21:37:37 -0300

    level 2 almost complete

---
+++ Assets/Scripts/Objects/Saw.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Saw : DamagePlayer
+{
+    public GameObject[] waypoints;
+    public float speed;
+    private int current = 0;
+    [SerializeField] private float wRadius = .5f;
+    private void Update()
+    {
+        if(Vector2.Distance(waypoints[current].transform.position, transform.position) < wRadius)
+        {
+            current++;
+            if(current >= waypoints.Length)
+            {
+                current = 0;
+            }
+        }
+        transform.position = Vector2.MoveTowards(transform.position, waypoints[current].transform.position, Time.deltaTime * speed);
+    }
+
+}
