41224f360cede2a2737828388d3cac37a8790c77
bigassopper <deathdiec@gmail.com>
2021-10-03 22:42:54 -0300

    game before cutscene

--- Assets/Scripts/Objects/DamagePlayer.cs
+++ Assets/Scripts/Objects/DamagePlayer.cs
@@ -6,12 +6,17 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class DamagePlayer : MonoBehaviour
 {
+    Player player;
     private void OnTriggerEnter2D(Collider2D other) {
-        var player = other.GetComponent<Player>();
+        player = other.GetComponent<Player>();
         if(player == null)return;
         AudioController.Instance.PlaySound("deathSoundTrue");
         CameraFollow.Instance.OnCameraShake(.25f, 1f);
-        this.gameObject.SpawnParticle("DeathParticle",  player.transform, Vector3.zero);
+        this.gameObject.SpawnParticle("DeathParticle",  player.transform, Vector3.zero, Restart);
+        player.gameObject.SetActive(false);
+    }
+    public void Restart(){
+        if(player != null)player.gameObject.SetActive(true);
         Engine.Instance.Restart();
     }
 }
